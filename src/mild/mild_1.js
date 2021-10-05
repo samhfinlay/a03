cd1b70b3aa65caed7af2fb054398e77371dd5ca5
Sam Finlay <69521209+samhfinlay@users.noreply.github.com>
2021-10-05 19:29:12 -0400

    asd

--- src/mild/mild_1.js
+++ src/mild/mild_1.js
@@ -9,7 +9,7 @@
  * see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
  */
 export function sumToString(a, b) {
-
+    console.log(a + " + " + b + " = " + a + b);
 }
 
 
