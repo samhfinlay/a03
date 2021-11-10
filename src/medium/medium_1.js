b6de8547c298d27ebfbe1cf927f8830e00dd04cd
Sam Finlay <69521209+samhfinlay@users.noreply.github.com>
2021-11-10 11:06:13 -0500

    asd

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -8,7 +8,11 @@ import {variance} from "./data/stats_helpers.js";
  * prototype functions. Very useful
  */
 export function getSum(array) {
-
+    var sum = 0;
+for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+}
+return sum;
 }
 
 
